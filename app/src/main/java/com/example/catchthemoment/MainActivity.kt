055b511f605c01cd041bb993be8c6f5ce6374cb1
VoidMiner <dubrovinss91@gmail.com>
2023-11-06 01:13:40 +0300

    Задание 1

--- app/src/main/java/com/example/catchthemoment/MainActivity.kt
+++ app/src/main/java/com/example/catchthemoment/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.catchthemoment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val readyButton = findViewById<Button>(R.id.readyButton)
+        readyButton.setOnClickListener {
+            // Действия при нажатии на кнопку "Готово"
+            val text = findViewById<EditText>(R.id.textEditText).text.toString()
+            // Здесь можно выполнить дополнительные действия с введенным текстом
+            Toast.makeText(this, "Текст: $text", Toast.LENGTH_SHORT).show()
+        }
     }
-    //тест гитхаб 2
 }
+
